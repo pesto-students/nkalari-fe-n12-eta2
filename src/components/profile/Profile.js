3fcedd3ea0a526f1e60fa1eed6df8824a92c1cad
jagan607 <jaganmohan19972016@gmail.com>
2022-01-11 23:55:16 +0530

    profile page updated

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -28,11 +28,15 @@ function Profile(props) {
 
         <div className="featuring">
 
-        <div className="flex flex-wrap justify-center">
-        <div className="w-8/12 sm:w-3/12 px-8 my-20">
-            <img src={profile?.image} alt="..." className="shadow-lg rounded-full max-w-full h-auto align-middle border-none" />
+        <div className="flex flex-wrap justify-center" >
+        <div className="w-8/12 sm:w-3/12 px-8 my-24">
+            <img src="https://source.unsplash.com/x_8oJhYU31k/200x200" alt="..." className="shadow-lg rounded-full max-w-full h-auto align-middle border-none" />
+            
         </div>
+        {/* <div className="following-count"><span>130 Following</span></div> */}
+       
     </div>
+    
        
 
 
@@ -41,12 +45,68 @@ function Profile(props) {
 
         <div className="attended-gigs">
 
+        <div className="box-transaction flex my-4 ml-16">
+                      <div className="flex items-center h-20 justify-between w-full px-10 font-semibold">
+                        <div className="flex flex-col ">
+                          <p className="text-white text-3l">{"John Doe"}</p>
+                          <p className="text-white text-2l">{"Next Gig"}</p>
+                        </div>
+                        <div className="flex justify-center">
+                          <p className="text-white text-2l">{"Thursday 2:00pm"}</p>
+                          {/* <img src={Diamonds} className="w-8" /> */}
+                        </div>
+                      </div>
+                    </div>
+
         </div>
 
 
         </div>
 
         <div class="profile-vertical-line"></div>
+
+        <div className="follow-list">
+  
+                    <div className="box-transaction flex my-4 ml-16">
+                      <div className="flex items-center h-20 justify-between w-full px-10 font-semibold">
+                        <div className="flex flex-col justify-center items-center">
+                          <p className="text-white text-3l">{"John Doe"}</p>
+                          <p className="text-white text-2l">{"Next Gig"}</p>
+                        </div>
+                        <div className="flex justify-center">
+                          <p className="text-white text-2l">{"Thursday 2:00pm"}</p>
+                          {/* <img src={Diamonds} className="w-8" /> */}
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="box-transaction flex my-4 ml-16">
+                      <div className="flex items-center h-20 justify-between w-full px-10 font-semibold">
+                        <div className="flex flex-col justify-center items-center">
+                          <p className="text-white text-3l">{"John Doe"}</p>
+                          <p className="text-white text-2l">{"Next Gig"}</p>
+                        </div>
+                        <div className="flex justify-center">
+                          <p className="text-white text-2l">{"Thursday 2:00pm"}</p>
+                          {/* <img src={Diamonds} className="w-8" /> */}
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="box-transaction flex my-4 ml-16">
+                      <div className="flex items-center h-20 justify-between w-full px-10 font-semibold">
+                        <div className="flex flex-col justify-center items-center">
+                          <p className="text-white text-3l">{"John Doe"}</p>
+                          <p className="text-white text-2l">{"Next Gig"}</p>
+                        </div>
+                        <div className="flex justify-center">
+                          <p className="text-white text-2l">{"Thursday 2:00pm"}</p>
+                          {/* <img src={Diamonds} className="w-8" /> */}
+                        </div>
+                      </div>
+                    </div>
+              
+        </div>
 
         
     </div>
