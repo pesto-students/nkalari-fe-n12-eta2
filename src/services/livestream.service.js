5011a2cee74620c28c0d4a7a2b49be1de24009c0
Avi x <axios47@gmail.com>
2022-02-05 23:03:13 +0530

    livestream integration

---
+++ src/services/livestream.service.js
@@ -0,0 +1,14 @@
+// join a stream
+export const joinStream = (user,channel) => {
+  return
+}
+
+// go live
+export const goLive = (host,channel) => {
+  return
+}
+
+// leave stream
+
+
+// end stream
