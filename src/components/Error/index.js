8cf319ea6d6a6333db336809858203823b8278d6
Puneet Tiwari <puneettiwari61@gmail.com>
2022-02-06 10:54:45 +0530

    fixed error page

--- src/components/Error/index.js
+++ src/components/Error/index.js
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Loader from "../Loader";
 import error from "./../../images/error.gif";
 
 function Error() {
-  return (
+  const [showLoader, setShowLoader] = useState(true);
+  useEffect(() => {
+    setTimeout(() => setShowLoader(false), 4000);
+  }, []);
+  return showLoader ? (
+    <Loader />
+  ) : (
     <div className="flex justify-center items-center h-full w-full">
       <img src={error} />
     </div>
